3512dc51e0b2e7c788114af911527d6b8cae0aca
Karthik Vasudevan <mithralaya@icloud.com>
2017-04-11 16:15:34 +0100

    improve naming and extend native types

--- src/type/niType.js
+++ src/type/niType.js
@@ -67,11 +67,11 @@ global.NICustom     = 'Custom';
 //global functions
 
 /**
- * [global niType]
+ * [global niTypeOf]
  * @param suspect
  * @return {String}
  */
-global.niType       = (suspect) => {
+global.niTypeOf     = (suspect) => {
   let trueType = getTrueType(suspect);
   if (trueType === 'String') {
     return getStringSubType(suspect);
@@ -83,55 +83,75 @@ global.niType       = (suspect) => {
 };
 
 /**
- * [global niTrueType]
+ * [global niTrueTypeOf]
  * Get the actual javascript type
  * @param suspect
  * @returns {String}
  */
-global.niTrueType   = (suspect) => {
+global.niTrueTypeOf = (suspect) => {
   return getTrueType(suspect);
 };
 
 /**
- * [global niCompare]
+ * [global extend]
  * Compares suspect with given type
- * @param suspect
- * @param compareWith
- * @param thirdArg // this argument depends on compareWith value (Array for NIEnum, RegExp for NICustom)
+ * @param thisArg suspect
+ * @param expectedType
+ * @param secondArg // this argument depends on expectedType value (Array for NIEnum, RegExp for NICustom)
  * @returns {boolean}
  */
-global.niCompare   = (suspect, compareWith, thirdArg = null) => {
-  let typeError = 'Expecting thridArg to be of type';
-  if (compareWith === NIEnum) {
+var extend = function(expectedType, secondArg = null) {
+  let typeError = 'Expecting secondArg to be of type';
+  if (expectedType === NIEnum) {
     //enum checker
-    if (thirdArg === null) {
-      thirdArg = [];
+    if (secondArg === null) {
+      secondArg = [];
     }
-    if (getTrueType(thirdArg) === NIArray) {
-      return (thirdArg.indexOf(suspect) >= 0);
-    } else {
-      throw new TypeError(typeError + ' NIArray');
-    }
-  } else if (compareWith === NICustom) {
+    if (getTrueType(secondArg) === NIArray) {
+      return (secondArg.indexOf(this) >= 0);
+    } else throw new TypeError(typeError + ' NIArray');
+  } else if (expectedType === NICustom) {
     //custom regex checker
-    if (getTrueType(thirdArg) === NIRegExp) {
-      return thirdArg.test(suspect);
-    } else {
-      throw new TypeError(typeError + ' NIRegExp');
-    }
+    if (getTrueType(secondArg) === NIRegExp) {
+      return secondArg.test(this);
+    } else throw new TypeError(typeError + ' NIRegExp');
   } else {
     //rest of it
-    if (niType(suspect) === compareWith) {
+    if (niTypeOf(this) === expectedType) {
       //check sub type
       return true;
-    } else if (getTrueType(suspect) === compareWith) {
-      //if its not satisfied with sub type, check with parent type.
-      return true;
-    } else {
-      return false;
-    }
+    } else return getTrueType(this) === expectedType;
   }
 };
+
+global.niIsOfType   = (suspect, expectedType, secondArg) => {
+  return extend.call(suspect, expectedType, secondArg);
+};
+
+if (!String.prototype.niIsOfType) {
+  String.prototype.niIsOfType = extend;
+}
+if (!Number.prototype.niIsOfType) {
+  Number.prototype.niIsOfType = extend;
+}
+if (!Array.prototype.niIsOfType) {
+  Array.prototype.niIsOfType = extend;
+}
+if (!Object.prototype.niIsOfType) {
+  Object.prototype.niIsOfType = extend;
+}
+if (!Boolean.prototype.niIsOfType) {
+  Boolean.prototype.niIsOfType = extend;
+}
+if (!Date.prototype.niIsOfType) {
+  Date.prototype.niIsOfType = extend;
+}
+if (!RegExp.prototype.niIsOfType) {
+  RegExp.prototype.niIsOfType = extend;
+}
+if (!Function.prototype.niIsOfType) {
+  Function.prototype.niIsOfType = extend;
+}
 
 //private functions
 
@@ -154,7 +174,7 @@ function getTrueType(suspect) {
 function isJsonString(str) {
   try {
     let val = JSON.parse(str);
-    let valTrueType = niTrueType(val);
+    let valTrueType = getTrueType(val);
     if (valTrueType === NINull || valTrueType === NINumber || valTrueType === NIBoolean) {
       return false;
     }
@@ -189,9 +209,7 @@ function getStringSubType(suspect) {
     return NICreditCard;
   } else if (isJsonString(suspect)) {
     return NIJSON;
-  } else {
-    return NIString;
-  }
+  } else return NIString;
 }
 
 /**
@@ -209,7 +227,5 @@ function getNumberSubType(suspect) {
     return NIFloat;
   } else if (suspect % 1 === 0 && Number.isSafeInteger(suspect)) {
     return NIInt;
-  } else {
-    return NINumber;
-  }
+  } else return NINumber;
 }
---
+++ test/type/enum.spec.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Karthik Vasudevan on 10/04/2017.
+ * License: MIT
+ */
+//enum doesn't have type or trueType checker
+//enum can only be validated using niCompareWith
