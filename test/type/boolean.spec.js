332250e89126a5985293e6e45f88f49304009242
Karthik Vasudevan <karthik@wearepercent.com>
2017-07-20 22:00:12 +0100

    added boolean test spec

--- test/type/boolean.spec.js
+++ test/type/boolean.spec.js
@@ -22,16 +22,51 @@ describe('NITypeOf', () => {
       assert.notEqual(niTypeOf({}), NIBoolean);
       assert.notEqual(niTypeOf(undefined), NIBoolean);
       assert.notEqual(niTypeOf(null), NIBoolean);
+      assert.notEqual(niTypeOf(function() {}), NIBoolean);
     });
   });
 });
 describe('NITrueTypeOf', () => {
   describe('NIBoolean', () => {
-
+    it('Should be a boolean', function() {
+      assert.equal(niTrueTypeOf(true), NIBoolean);
+      assert.equal(niTrueTypeOf(false), NIBoolean);
+      assert.equal(niTrueTypeOf(new Boolean()), NIBoolean);
+      assert.equal(niTrueTypeOf(new Boolean(1)), NIBoolean);
+    });
+    it('Should not be a boolean', function() {
+      assert.notEqual(niTrueTypeOf(1), NIBoolean);
+      assert.notEqual(niTrueTypeOf(0), NIBoolean);
+      assert.notEqual(niTrueTypeOf('sdfwef'), NIBoolean);
+      assert.notEqual(niTrueTypeOf([]), NIBoolean);
+      assert.notEqual(niTrueTypeOf({}), NIBoolean);
+      assert.notEqual(niTrueTypeOf(undefined), NIBoolean);
+      assert.notEqual(niTrueTypeOf(null), NIBoolean);
+      assert.notEqual(niTrueTypeOf(function() {}), NIBoolean);
+    });
   });
 });
 describe('Native Extend', () => {
   describe('NIBoolean', () => {
-
+    it('Should be a boolean', function() {
+      assert.isTrue(new Boolean(1).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean(0).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean(undefined).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean(null).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean({}).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean([]).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean(NaN).niIsOfType(NIBoolean));
+      assert.isTrue(new Boolean(() => {}).niIsOfType(NIBoolean));
+      assert.isTrue(true.niIsOfType(NIBoolean));
+      assert.isTrue(false.niIsOfType(NIBoolean));
+    });
+    it('Should not be a boolean', function() {
+      assert.isFalse('sdfsdf'.niIsOfType(NIBoolean));
+      assert.isFalse((1).niIsOfType(NIBoolean));
+      assert.isFalse((23423423424234).niIsOfType(NIBoolean));
+      assert.isFalse({}.niIsOfType(NIBoolean));
+      assert.isFalse([].niIsOfType(NIBoolean));
+      assert.isFalse((function() {}).niIsOfType(NIBoolean));
+    });
   });
 });
